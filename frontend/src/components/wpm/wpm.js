9d0e6c683351087b9e23f6c05ac0d4b573c7ecf1
darrenyong <contact.darrenyong@gmail.com>
2019-04-01 15:44:33 -0700

    Add WPM component

---
+++ frontend/src/components/wpm/wpm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export class Wpm extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        wordsPerMin: (this.props.typedEntries/5)/(this.props.timeElapsed/60) || 0
+      }
+    }
+
+    render () {
+      console.log(this.props);
+      return (
+        <h1>{this.state.wordsPerMin}</h1>
+      )
+    }
+}
