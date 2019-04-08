e5e51a4821e2e8ed7d4a4a08b708700642d18882
James <james@jameshawkins.com>
2019-04-07 19:58:04 -0700

    Add countdown to game screen

--- frontend/src/components/game/game.js
+++ frontend/src/components/game/game.js
@@ -14,6 +14,7 @@ class Game extends React.Component {
         typedEntries: 0,
         wordsPerMin: 0,
         mistakes: 0,
+        countdown: true,
         gameWon: false,
         interval: "",
         ignoreKeys: ['Alt', 'Meta', 'Tab', 'Control','Shift','CapsLock', 'Enter', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown', 'Home', 'End', 'PageUp', 'PageDown']
@@ -53,6 +54,8 @@ class Game extends React.Component {
       
     }
 
+
+
     componentWillUnmount() {
       document.removeEventListener("keydown", this.detectKeyPresses);
     }
@@ -62,6 +65,12 @@ class Game extends React.Component {
       let newIncorrectLetters = this.state.incorrectLetters;
       let newCorrectLetters = this.state.correctLetters;
       let nextLetter;
+      console.log(this.state.countdown);
+      if (this.state.countdown){
+        if (e.key === 'Enter'){
+          this.setState( {countdown: false })
+        }
+      } else {
       if (this.state.typedEntries === 0){
         this.setState( { interval: setInterval(this.incrementTime, 10), timeElapsed: 0.01, });
       }
@@ -87,8 +96,8 @@ class Game extends React.Component {
         correctLetters: newCorrectLetters,
         phrase: newPhrase,
         typedEntries: this.state.typedEntries+1,
-        
       });
+    }
     }
 
     incrementTime() {
@@ -101,23 +110,36 @@ class Game extends React.Component {
 
     render () {      
 
+      let countdown = (
+        <>
+          <div className="countdown flex">
+            <h1>Press <span>Enter</span> to get started</h1>
+          </div>
+        </>
+      )
+
+      let gameRender = (
+            <>
+            <div className="answer-phrase flex">
+                        <pre>
+                          { this.state.correctLetters.length ? (<span className="green">{ this.state.correctLetters.join("")}</span>) : "" }
+                          { this.state.incorrectLetters.length ? (<span className="red">{ this.state.incorrectLetters.join("") }</span>) : "" }
+                          <span className="regular" >{ this.state.phrase.join("") || ""} </span>
+                        </pre>
+                    </div>
+                    <div className={`game-stats flex ${this.state.gameWon ? "finished": ""}`}>
+                      <p className={`wpm flex`}>Words per minute: {this.state.wordsPerMin}</p>
+                      <p className={`wpm flex`}>Time: {Math.floor(this.state.timeElapsed)} seconds</p>
+                      <p className={`wpm flex`}>Accuracy: { `${Math.max( Math.floor((this.state.correctLetters.length - this.state.mistakes) / (this.state.correctLetters.length || 0.0001) * 100), 0)}%` }</p>
+              </div>
+            </>
+          )
+
         return (
           <>
             <div className="game-area-parent flex-column">
               <div className="game-area">
-                <div className="answer-phrase flex">
-                    <pre>
-                      { this.state.correctLetters.length ? (<span className="green">{ this.state.correctLetters.join("")}</span>) : "" }
-                      { this.state.incorrectLetters.length ? (<span className="red">{ this.state.incorrectLetters.join("") }</span>) : "" }
-                      <span className="regular" >{ this.state.phrase.join("") || ""} </span>
-                    </pre>
-                </div>
-                  {/* { this.state.gameWon ? <p className="game-congrats">Mission Accomplished!</p> : <pre><p> </p></pre> } */}
-                <div className={`game-stats flex ${this.state.gameWon ? "finished": ""}`}>
-                  <p className={`wpm flex`}>Words per minute: {this.state.wordsPerMin}</p>
-                  <p className={`wpm flex`}>Time: {Math.floor(this.state.timeElapsed)} seconds</p>
-                  <p className={`wpm flex`}>Accuracy: { `${Math.max( Math.floor((this.state.correctLetters.length - this.state.mistakes) / (this.state.correctLetters.length || 0.0001) * 100), 0)}%` }</p>
-                </div>
+                { this.state.countdown ? countdown : gameRender }
               </div>
               <div className="progress-meter flex">
                   <img className="earth" alt='earth' src="./assets/earth.png"/>
