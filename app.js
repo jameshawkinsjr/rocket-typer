f47fb874b11c61940ec36928e6702e6f7bd7f0df
James <james@jameshawkins.com>
2019-04-09 16:15:03 -0700

    Attempt to fix heroku routing

--- app.js
+++ app.js
@@ -13,9 +13,12 @@ const io = require('socket.io').listen(server);
 const users = require('./routes/api/users');
 const races = require('./routes/api/races');
 
+app.use('/api/users', users);
+app.use('/api/races', races);
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'), function(err) {
       if (err) {
         res.status(500).send(err)
@@ -72,3 +75,4 @@ app.use('/api/races', races);
 server.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
+
