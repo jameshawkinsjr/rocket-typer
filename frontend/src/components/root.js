b8f8082464e2a3f39c7644217d9ed6a77db087cf
James <james@jameshawkins.com>
2019-04-09 10:06:04 -0700

    Extract socket to separate context

--- frontend/src/components/root.js
+++ frontend/src/components/root.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app';
+import SocketContext from '../api/socket-context';
+import openSocket from 'socket.io-client';
+
+const url = `${window.location.hostname}:${window.location.port}`;
+const socket = openSocket(url);
 
 const Root = ( { store } ) => (
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
+    <SocketContext.Provider value={socket}>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </SocketContext.Provider>
 );
 
 export default Root;
